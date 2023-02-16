ea71d3be0a70c210c8deeed72765eb244fec1dcf
Alexey Rytsku <topelven1234567890@gmail.com>
2023-02-16 18:33:06 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // массив для игровой доски
+    char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+    // массив для игры
+    char turn = 'X';
+    bool draw = false;
+
+    return 0;
+}
